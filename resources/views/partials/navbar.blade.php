3d714cd089a3f4e570d2aa1884e5d66a847b638a
Wojtek2006 <w-jackowiak@wp.pl>
2025-03-20 13:19:52 +0100

    init

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
+        <a class="navbar-brand" href="index.php">Zawody pływania</a>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Strona główna</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contenders.php">Zawodnicy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="teams.php">Drużyny</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="competitions.php">Zawody</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
