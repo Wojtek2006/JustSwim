e20b8f4c44ae88e0d807e59e4d6f113061fc917f
vvlfn <szkolapawel16@gmail.com>
2025-03-23 13:57:47 +0100

    huh

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,5 @@
 @extends("base/baseapp")
 
 @section("content")
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor tempore error nemo amet eos consectetur velit officiis quos! Dolore laudantium saepe suscipit itaque rerum exercitationem, sit molestiae iste optio esse et in quis, possimus ipsam laboriosam, voluptate voluptatibus. Officia, unde? Nostrum nemo fugiat iste omnis odio saepe tempore. Ratione, quaerat.</p>
+    @include("partials/landing/landing")
 @endsection
